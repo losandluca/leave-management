6abf22ce422aaa88c673a2bfbf73ca8f35e77cac
Carlos Alcazar <calcazar@mdscomm.onmicrosoft.com>
2022-03-15 14:59:43 -0700

    Replaced UserIdentity with Employee, created view for leave allocation.

--- Models/LeaveAllocationViewModel.cs
+++ Models/LeaveAllocationViewModel.cs
@@ -11,9 +11,10 @@ namespace leave_management.Models
     {
 
         public int Id { get; set; }
-        [Required]
         public int NumberOfDays { get; set; }
         public DateTime DateCreated { get; set; }
+
+        public int Period { get; set; }
         public EmployeeViewModel Employee { get; set; }
         public string EmployeeId { get; set; }
         public LeaveTypeViewModel LeaveType { get; set; }
@@ -21,5 +22,13 @@ namespace leave_management.Models
 
         public IEnumerable<SelectListItem> Employees { get; set; }
         public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+
+
+    public class CreateLeaveAllocationViewmodel
+    {
+        public int NumberUpdated { get; set; }
+        
+        public List<LeaveTypeViewModel> LeaveTypes { get; set; }
     }
 }
--- Models/LeaveTypeViewModel.cs
+++ Models/LeaveTypeViewModel.cs
@@ -11,6 +11,11 @@ namespace leave_management.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Default Number Of Days")]
+        [Range(1,25, ErrorMessage = "Please Enter a Valid Number")]
+        public int DefaultDays { get; set; }
         [Display(Name="Date Created")]
         public DateTime? DateCreated { get; set; }
     }
